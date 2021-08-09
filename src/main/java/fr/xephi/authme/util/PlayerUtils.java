1ae6542a47d743718ee0e275d081765b9d053e54
xzzpig <xzzpig@fae2ly.com>
2021-08-09 15:38:04 +0800

    修改IP从Gayser获取

--- src/main/java/fr/xephi/authme/util/PlayerUtils.java
+++ src/main/java/fr/xephi/authme/util/PlayerUtils.java
@@ -1,6 +1,8 @@
 package fr.xephi.authme.util;
 
 import org.bukkit.entity.Player;
+import org.geysermc.connector.GeyserConnector;
+import org.geysermc.connector.network.session.GeyserSession;
 
 /**
  * Player utilities.
@@ -18,6 +20,9 @@ public final class PlayerUtils {
      * @return The player's IP address
      */
     public static String getPlayerIp(Player player) {
+        String geyserIp = getGeyserIp(player);
+        if (geyserIp != null)
+            return geyserIp;
         return player.getAddress().getAddress().getHostAddress();
     }
 
@@ -29,6 +34,18 @@ public final class PlayerUtils {
      */
     public static boolean isNpc(Player player) {
         return player.hasMetadata("NPC");
+    }
+
+    public static String getGeyserIp(Player player) {
+        if (player == null)
+            return null;
+        GeyserConnector connector = GeyserConnector.getInstance();
+        if (connector == null)
+            return null;
+        GeyserSession session = connector.getPlayerByUuid(player.getUniqueId());
+        if (session == null)
+            return null;
+        return session.getSocketAddress().getAddress().getHostAddress();
     }
 
 }
